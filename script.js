e293a9aeee8e524d34d8f493d4262f7bb9658bb8
Valentina1891 <vvaldiviam@uft.edu>
2025-09-05 12:03:59 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('red').textContent = "GoodBye"
+
+document.querySelectorAll('h1')[1].style.color = "orange"
+
+document.getElementById("clickable").addEventListener('click',function(){
+    this.style.color = 'brown'
+})
